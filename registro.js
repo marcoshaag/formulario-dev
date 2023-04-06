9e97f3e2cfe68caf537ccc456df77d797096f128
marcoshaag <marcoshaag2019@gmail.com>
2023-04-05 23:13:49 -0300

    Teste de Js e ajuste tela-registro

--- registro.js
+++ registro.js
@@ -3,9 +3,10 @@ function cadastro(){
     var login = document.getElementById('email').value;
     var nome = document.getElementById('name').value;
 
-    if(login > '' && nome > ''){
+
+    if(login.length > 1 && nome.length > 1){
         alert('Sucesso');
-        location.href= "tela-registro.html";
+        location.href= "./tela-registro.html";
     }else{
         alert('Incoreto');
     }
